590e2dfdeec7a6fe503d3682b315deff205bcd33
Isis <blancoisis1@gmail.com>
2022-03-25 13:24:07 -0400

    commit tree 8d47d0231f928304361373cbcae446f627ecdb59

--- index.js
+++ index.js
@@ -1,1 +1,66 @@
-// Write your solution here!
+const cats = ["Milo", "Otis", "Garfield"];
+
+function destructivelyAppendCat() {
+    cats.push("Ralph");
+}
+
+function destructivelyPrependCat() {
+    cats.unshift("Bob")
+}
+
+function destructiveRemoveLastCat() {
+    cats.pop()
+}
+
+function destructiveRemoveFirstCat() {
+    cats.shift()
+}
+
+destructivelyPrependCat ("Bob");
+
+cats.length = 0;
+
+    cats.push("Milo", "Otis", "Garfield")
+
+    function destructivelyRemoveLastCat () {
+        cats.pop();
+    }
+
+cats.length = 0;
+
+    cats.push("Milo", "Otis", "Garfield");
+
+function destructivelyRemoveFirstCat () {
+    cats.shift();
+}
+
+cats.length = 0;
+
+    cats.push("Milo", "Otis", "Garfield"); 
+
+function appendCat(name) {
+let copyCats = [...cats];
+    return copyCats, name;
+}
+appendCat ("Broom")
+
+cats.length = 0;
+
+    cats.push("Milo", "Otis", "Garfield")
+
+function prependCat (name) {
+    let copyCats = [name, ...cats];
+    return copyCats;
+}
+
+prependCat ("Arnold")
+
+cats.length = 0;
+
+    cats.push("Milo", "Otis", "Garfield")
+
+function removeLastCat() {
+    let copyCats = [...cats];
+    let copyCats2 = [copyCats.pop()];
+    return copyCats2;
+}
